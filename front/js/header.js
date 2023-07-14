7de51cabfd7f19902151a6346e28acfd225d7c34
EuniceHwang <appletart177@gmail.com>
2023-07-15 00:03:03 +0900

    url change

--- front/js/header.js
+++ front/js/header.js
@@ -12,7 +12,7 @@
 
 */
 
-let url = "https://www.youtubetasty.store:3001";
+let url = "https://www.youtubetasty.store";
 
 // 1. 로컬스토리지에서 x-access-token 확인
 const jwt = localStorage.getItem("x-access-token");
